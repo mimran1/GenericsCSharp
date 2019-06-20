8ead74a86bdfa3f111bff629458252062ad8c617
Mohammad <imran2408@gmail.com>
2019-06-20 12:33:04 -0400

    First commit

---
+++ Generics/GenericList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    /// <summary>
+    /// Generics allow to implement a concept like sorting. It leaves the data out of it by providing generic parameter (in our case T).
+    /// So we can write a simple stack program, that can stack anything. The idea (stacking) is implemented. Data is left for user to provide
+    /// Following code was adopted from: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/generics/index
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+
+    public class GenericList<T>
+    {
+        public class Node
+        {
+            private T data;
+            private Node next;
+            public Node(T t)
+            {
+                data = t;
+            }
+
+            public Node Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+
+            public T Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+
+        }
+
+        private Node head;
+
+        public GenericList()
+        {
+            head = null;
+        }
+
+        public void AddHead(T t)
+        {
+            Node n = new Node(t);
+            n.Next = head;
+            head = n;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node curr = head;
+            while (curr != null)
+            {
+                yield return curr.Data;
+                curr = curr.Next;
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create generic class with int
+            GenericList<int> listInt = new GenericList<int>();
+            listInt.AddHead(1);
+            listInt.AddHead(2);
+            listInt.AddHead(3);
+            listInt.AddHead(4);
+
+            System.Console.Write("Stack of integer: ");
+            foreach (int i in listInt)
+            {
+                System.Console.Write(i + " ");
+            }
+            System.Console.WriteLine("\n");
+            
+
+            //Create generic class with String
+
+            GenericList<string> listString = new GenericList<string>();
+            listString.AddHead("A");
+            listString.AddHead("B");
+            listString.AddHead("C");
+            listString.AddHead("D");
+
+            System.Console.Write("Stack of string: ");
+            foreach (string i in listString)
+            {
+                System.Console.Write(i + " ");
+            }
+
+            System.Console.WriteLine("\n\nDone.");
+            Console.ReadKey();
+
+        }
+    }
+}
